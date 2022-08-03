c1d2da3bf1ca9197aad4125fdd8833b7e94e18cf
afdezcl <adrifdez_@uma.es>
2022-08-03 23:01:59 +0200

    Update folder project

---
+++ frontend-vapers/data/vaper.js
@@ -0,0 +1,44 @@
+export const Vapers = [
+  {
+      "title": "Sandia helada con fresas",
+      "description": "Si recuerdas lo bueno del verano y la primavera, esta combinación de Sandia helada con las mejores fresas de temporada te dejará sin palabras, ¡atrévete a probarlo y nos cuentas!",
+      "tags": ["Sandia", "Hielo", "Fresa"],
+      "image": "https://drive.google.com/uc?id=1-_DM-FcwCewKdbwVIl7DG_Xza35cUQag"
+  },
+  {
+      "title": "Cocacola Congelada",
+      "description": "Esos momentos cuando bebes una cocacola congeladiiisima y aparece esa sonrisa de satisfacción, ahora puedes conseguirlo en cualquier momento de calor, ¡Te lo aseguro! COCACOLA HELADA",
+      "tags": ["Cocacola", "Hielo"],
+      "image": "https://drive.google.com/uc?id=1-4uvM5vnUUwgc9lZpwnkMbRr06qsHsji"
+  },
+  {
+      "title": "Melón con Manzana FUJI",
+      "description": "Si eres amante del melón, o no, esta increible combinación con una manzana desarrollada por cultivadores de la estación de Fujisaka, ¡hará que quieras 50 como este!",
+      "tags": ["Melón", "Manzana", "Fuji"],
+      "image": "https://drive.google.com/uc?id=1-ZTNiJuz2FPiqTpoXcYL_xi9GXQNYVtR"
+  },
+  {
+      "title": "Fruta de la PASIÓN",
+      "description": "Sabemos que te llama la atención, pero no te atreves, esta combinación de fruta del dragón, kiwi, mora y piña hará que todo el que te rodea quiera probar esa ¡BARBARIDAD!",
+      "tags": ["Fruta-Pasión", "Kiwi", "Mora", "Piña"],
+      "image": "https://drive.google.com/uc?id=1EO9-Ys13w2e47ejdLjNrz9Ro5m8sEJ8f"
+  },
+  {
+      "title": "Menta Polar",
+      "description": "¡Abrígate! Esta menta procedente de la mismísima Antártida, extraida por los mejores investigadores de cubitos de hielo ¡DE TODA LA GALAXIA!",
+      "tags": ["Hielo", "Polar"],
+      "image": "https://drive.google.com/uc?id=1-qI9Qo6WOF8soPZ8xYdXMU0gyg4ebQcB"
+  },
+  {
+      "title": "Limonada de Frambuesa-Limón",
+      "description": "¡Para qué más! Esta limonada no solo hará que cierres los ojos de placer, necesitarás más y remomerar todos los momentos buenos del verano",
+      "tags": ["Limonada", "Frambuesa", "Limón"],
+      "image": "https://drive.google.com/uc?id=1-6pI5ct1m2QnprZy-KjHcHESrHJdflj8"
+  },
+  {
+      "title": "Helado de Arándanos Hielo",
+      "description": "Los mejores arándanos del mundo, juntos al hielo antártico ¡harán que te desmayes de placer!",
+      "tags": ["Helado", "Arándanos", "Hielo"],
+      "image": "https://drive.google.com/uc?id=1-ZD-6lQbkmT8hXG2Z2ekz3qJFOt3m80U"
+  }
+]
---
+++ frontend-vapers/src/components/Card.jsx
@@ -0,0 +1,20 @@
+export const Card = ({title, description, image, tags}) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-xl">
+      <img className="w-full" src={image} alt="Sunset in the mountains" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">
+          {description}
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        {
+          tags.map(tag => 
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
+          )
+        }
+      </div>
+    </div>
+  )
+}
