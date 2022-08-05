e978d76966eed20243089f169a94e1cc752bafc8
almelism <ismael.almellon@externals.adidas.com>
2022-08-05 11:26:30 +0200

    Update-Passion-Fruit-image-v2

--- frontend-vapers/data/vaper.js
+++ frontend-vapers/data/vaper.js
@@ -21,7 +21,7 @@ export const Vapers = [
       "title": "Fruta de la PASIÓN",
       "description": "Sabemos que te llama la atención, pero no te atreves, esta combinación de fruta del dragón, kiwi, mora y piña hará que todo el que te rodea quiera probar esa ¡BARBARIDAD!",
       "tags": ["Fruta-Pasión", "Kiwi", "Mora", "Piña"],
-      "image": "http://drive.google.com/uc?id=1EO9-Ys13w2e47ejdLjNrz9Ro5m8sEJ8f"
+      "image": "http://drive.google.com/uc?id=17R7fWdwq1_rkhQl4yLrcOUPD8-gvVi9g"
   },
   {
       "title": "Menta Polar",
