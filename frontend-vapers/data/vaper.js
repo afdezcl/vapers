4bae7f46e5b5d0672bc34393dab115e1d2f84a5a
almelism <ismael.almellon@externals.adidas.com>
2022-08-05 12:04:47 +0200

    Update pods description

--- frontend-vapers/data/vaper.js
+++ frontend-vapers/data/vaper.js
@@ -1,43 +1,43 @@
 export const Vapers = [
   {
-      "title": "Sandia Helada con Fresas",
-      "description": "Si recuerdas lo bueno del verano y la primavera, esta combinación de Sandia helada con las mejores fresas de temporada te dejará sin palabras, ¡atrévete a probarlo y nos cuentas!",
-      "tags": ["Sandia", "Hielo", "Fresa"],
+      "title": "Sandía Helada con Fresas",
+      "description": "Lo BUENO del verano y la primavera JUNTOS, esta combinación de Sandía Helada con las mejores FRESAS de temporada te dejará sin palabras, ¡atrévete a probarlo y nos cuentas!",
+      "tags": ["Sandía", "Hielo", "Fresa"],
       "image": "https://drive.google.com/uc?id=1-_DM-FcwCewKdbwVIl7DG_Xza35cUQag"
   },
   {
       "title": "Refresco de Cocacola Hielo",
-      "description": "Esos momentos cuando bebes una cocacola congeladiiisima y aparece esa sonrisa de satisfacción, ahora puedes conseguirlo en cualquier momento de calor, ¡Te lo aseguro! COCACOLA HELADA",
+      "description": "¿A quién no le apetece una CocaCola FRIIIIIIA en verano?, ahora puedes beberte esa CocaCola en cualquier MOMENTO, ¡Te lo aseguro! COCACOLA HELADA",
       "tags": ["Cocacola", "Hielo"],
       "image": "https://drive.google.com/uc?id=10oN5Qu4U6Oue4Fmc4rOEoJ3kSbHrYbFf"
   },
   {
       "title": "Melón con Manzana FUJI",
-      "description": "Si eres amante del melón, o no, esta increíble combinación con una manzana desarrollada por cultivadores de la estación de Fujisaka, ¡hará que quieras 50 como este!",
+      "description": "Si eres amante del melón, o no, esta increíble combinación de MANZANA desarrollada por cultivadores de la estación de Fujisaka, ¡hará que quieras 50 como esta!",
       "tags": ["Melón", "Manzana", "Fuji"],
       "image": "https://drive.google.com/uc?id=1-ZTNiJuz2FPiqTpoXcYL_xi9GXQNYVtR"
   },
   {
       "title": "Fruta de la PASIÓN",
-      "description": "Sabemos que te llama la atención, pero no te atreves, esta combinación de fruta del dragón, kiwi, mora y piña hará que cualquiera necesite probar esa ¡BARBARIDAD!",
+      "description": "Sabemos que te llama la atención, pero no te atreves, prueba esta combinación de Fruta del DRAGÓN, KIWI, MORA y PIÑA, sin duda, hará que cualquiera QUIERA probar esa ¡BARBARIDAD!",
       "tags": ["Fruta-Pasión", "Kiwi", "Mora", "Piña"],
       "image": "http://drive.google.com/uc?id=17R7fWdwq1_rkhQl4yLrcOUPD8-gvVi9g"
   },
   {
       "title": "Menta Polar",
-      "description": "¡Abrígate! Esta menta procedente de la mismísima Antártida, extraida por los mejores investigadores de cubitos de hielo de ¡TODA LA GALAXIA!",
+      "description": "¡Abrígate! Esta MENTA procedente de la mismísima Antártida, extraída por los mejores investigadores de cubitos de HIELO de ¡TODA LA GALAXIA!",
       "tags": ["Hielo", "Polar"],
       "image": "https://drive.google.com/uc?id=1-qI9Qo6WOF8soPZ8xYdXMU0gyg4ebQcB"
   },
   {
       "title": "Limonada de Frambuesa-Limón",
-      "description": "¡Para qué más! Esta limonada no solo hará que cierres los ojos de placer, necesitarás más para remomerar todos los momentos buenos del verano",
+      "description": "¡Para qué más! Esta LIMONADA de FRAMBUESA, LIMÓN no solo hará que cierres los ojos de placer, necesitarás almenos ¡50 como esta!",
       "tags": ["Limonada", "Frambuesa", "Limón"],
       "image": "https://drive.google.com/uc?id=1-6pI5ct1m2QnprZy-KjHcHESrHJdflj8"
   },
   {
       "title": "Helado de Arándanos Hielo",
-      "description": "Los increibles Arándanos EUROPEOS juntos al hielo antártico ¡harán que te desmayes de placer!",
+      "description": "Los increíbles ARÁNDANOS EUROPEOS juntos al HIELO Antártico ¡harán que te desmayes de placer!",
       "tags": ["Helado-Arándanos", "Hielo"],
       "image": "https://drive.google.com/uc?id=1-ZD-6lQbkmT8hXG2Z2ekz3qJFOt3m80U"
   }
